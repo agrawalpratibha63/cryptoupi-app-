e60959e2c6f0dca5a9e14fc8237af42a6054f471
Pratibha Agrawal <agrawalpratibha63@gmail.com>
2026-07-09 07:12:21 +0530

    Add service worker for real PWA installability

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import RegisterSW from './register-sw';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,12 +15,15 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "CryptoUPI",
   description: "Send crypto straight to any UPI ID",
-  themeColor: "#5b3df6",
   manifest: "/manifest.json",
   icons: {
     icon: "/icon-192.png",
     apple: "/icon-192.png",
   },
+};
+
+export const viewport = {
+  themeColor: "#5b3df6",
 };
 
 export default function RootLayout({ children }) {
@@ -28,7 +32,7 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col"><RegisterSW />{children}</body>
     </html>
   );
 }
